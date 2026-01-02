2ed59ca971c75e50612dcd3cd1e707663e86b53b
Rajini <chekurirajini485@gmail.com>
2026-01-02 12:42:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Even Odd Game/even-odd-game.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static String utility(int n) {
+
+        if (n % 2 == 1) {
+            return "You";
+        } else {
+            return "Friend";
+        }
+    }
+}
