3197af80e8a2841c4746b7063c3c790a566ca392
Rajini <chekurirajini485@gmail.com>
2026-01-04 16:45:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Perfect Numbers/perfect-numbers.java
@@ -0,0 +1,18 @@
+class Solution {
+    static boolean isPerfect(int n) {
+        if (n <= 1) return false;
+
+        int sum = 1; // 1 is always a factor
+
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                sum += i;
+                if (i != n / i) {
+                    sum += n / i;
+                }
+            }
+        }
+
+        return sum == n;
+    }
+}
